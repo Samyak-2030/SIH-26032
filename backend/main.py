93f3dea5636305d5085f3bcbe200297af8fcbc17
noobvictor-web <tradersamyak115@gmail.com>
2026-09-05 23:08:31 +0530

    make first route

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class FarmerRegister(BaseModel):
+
+    fullName:str
+    mobile: str
+    email : str | None=None
+    state:str
+    district:str
+    village:str
+    landArea:str
+    crop:str
+    password:str
+
+@app.post('/farmers/register')
+
+def register_farmer(farmer:FarmerRegister):
+
+    return{
+        "message":"farmer registered successfully",
+        "farmer":farmer
+    }
